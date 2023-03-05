a68706390de16ad213c46ae88f5113b83e061bb7
siuynin <siuynin@gmail.com>
2023-03-05 23:55:20 +0700

    Add video demo

--- src/components/product/product-simple-form.tsx
+++ src/components/product/product-simple-form.tsx
@@ -82,6 +82,13 @@ export default function ProductSimpleForm({ initialValues }: IProps) {
           className="mb-5"
           disabled={isTranslateProduct}
         />
+        <Input
+          label={t("form:input-label-width")}
+          {...register("video")}
+          error={t(errors.width?.message!)}
+          variant="outline"
+          className="mb-5"
+        />
 
         <Input
           label={`${t('form:input-label-preview-url')}`}
